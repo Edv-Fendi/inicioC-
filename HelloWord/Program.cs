ce1370314f5eea7fd4001c8edb3756ec0826e4ff
Edvaldo Defendi <edvaldo.defendi@hotmail.com>
2022-11-08 09:43:59 -0300

    newConsole

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -13,15 +13,28 @@ namespace HelloWorld
 
             String texto;
             int idade = 22;
-
+            float altura = 1.89f;
+            bool fazAcademia = true;
+            var horaTrabalho = 4;
+            var curso = "Connect Biopark";
             texto = "Tudo bem?";
+            dynamic corFavorita = "red";
 
 
             Console.WriteLine("Hello World  ");
-            Console.WriteLine("Meu nome é Edvaldo :) ");
+            Console.WriteLine("\nMeu nome é Edvaldo :) ");
             Console.WriteLine(texto);
             Console.WriteLine("Eu tenho " + idade + " anos");
+            Console.WriteLine("Tenho " + altura + " de altura");
+            Console.WriteLine("Faz academia? " + fazAcademia);
+            Console.WriteLine("Curso: " + curso);
+            Console.WriteLine("Trabalho " + horaTrabalho + " horas por dia");
+            Console.WriteLine("Cor Favorita: " + corFavorita);
+            corFavorita = 4;
+            Console.WriteLine("Pra falar a verdade possuo umas: " + corFavorita + " cores favoritas :D");
 
+            texto = "\n\n\nObrigado por ler até aqui :)";
+            Console.WriteLine(texto);
             Console.ReadLine();
 
         }
