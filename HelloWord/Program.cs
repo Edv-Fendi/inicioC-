40f9a15642b174baef7edb06ec39bac7eaa6920a
Edvaldo Defendi <edvaldo.defendi@hotmail.com>
2022-11-08 08:55:32 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String texto;
+            int idade = 22;
+
+            texto = "Tudo bem?";
+
+
+            Console.WriteLine("Hello World  ");
+            Console.WriteLine("Meu nome é Edvaldo :) ");
+            Console.WriteLine(texto);
+            Console.WriteLine("Eu tenho " + idade + " anos");
+
+            Console.ReadLine();
+
+        }
+    }
+}
