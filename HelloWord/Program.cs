36ecaf14b6f63515829b9d9b84568c1bc8b3c5ed
Edvaldo Defendi <edvaldo.defendi@hotmail.com>
2022-11-09 10:45:55 -0300

    functionTimes

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -10,10 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
-            String texto, nomeAmigo;
-            const String nome= "Edvaldo";
-            int idade = 22;
+            string[] times = new string[3] { "Flamengo", "Real Madrid", "Brasil" };
+            string texto, nomeAmigo;
+            const string nome = "Edvaldo";
+            int idade = 22, preco = 0;
             float altura = 1.89f;
             bool fazAcademia = true;
             var horaTrabalho = 4;
@@ -24,41 +24,44 @@ namespace HelloWorld
 
 
             Console.WriteLine("Hello World  ");
-            Console.WriteLine("\nMeu nome é " + nome + " :) ");
+            Console.WriteLine($"\nMeu nome é {nome} :) ");
             Console.WriteLine(texto);
-            Console.WriteLine("Eu tenho " + idade + " anos");
-            Console.WriteLine("Tenho " + altura + " de altura");
-            Console.WriteLine("Faz academia? " + fazAcademia);
-            Console.WriteLine("Curso: " + curso);
-            Console.WriteLine("Trabalho " + horaTrabalho + " horas por dia");
+            Console.WriteLine($"Eu tenho {idade} anos");
+            Console.WriteLine($"Tenho {altura} de altura");
+            Console.WriteLine($"Faz academia? {fazAcademia}");
+            Console.WriteLine($"Curso: {curso}");
+            Console.WriteLine($"Trabalho {horaTrabalho} horas por dia");
             Console.WriteLine("Cor Favorita: " + corFavorita);
             corFavorita = 4;
-            Console.WriteLine("Pra falar a verdade possuo umas: " + corFavorita + " cores favoritas :D");
-            Console.WriteLine("Sou Flamenguista maluco e meu time tem: " + numeroTacasFlamengo + 
-                " titulos, contando apenas Copa do Brasil e Libertadores :D");
-            Console.WriteLine("Mas se for contar os outros trofeus deve dar uns "  + 
-                numeroTacasFlamengo * 5 + " ou mais hahahaha ");
+            Console.WriteLine($"Pra falar a verdade possuo umas: {corFavorita} cores favoritas :D");
+            Console.WriteLine($"Sou Flamenguista maluco e meu time tem: {numeroTacasFlamengo} " +
+                $"titulos, contando apenas Copa do Brasil e Libertadores :D");
+            Console.WriteLine($"Mas se for contar os outros trofeus deve dar uns " +
+                $"{numeroTacasFlamengo * 5} ou mais hahahaha ");
 
             texto = "\n\n\nObrigado por ler até aqui :)";
-            Console.Write("\n\n\nMas vamos falar sobre você agora!!!!") ;
+            Console.Write("\n\n\nMas vamos falar sobre você agora!!!!");
             Console.Write("\n\nQual o seu nome?\n");
             nomeAmigo = Console.ReadLine();
             Console.Write("\nQual a sua idade?\n");
             int idadeAmigo = int.Parse(Console.ReadLine());
             mostrarIdade(idade, idadeAmigo, nomeAmigo);
-            
-            Console.WriteLine("\n\nPara qual time voce torce?");
-            String timeAmigo = Console.ReadLine();
+
+            Console.WriteLine("\nPara qual time voce torce?");
+            string timeAmigo = Console.ReadLine();
             seuTime(timeAmigo);
+            gerarPreco(preco);
+            cadastrotimesFavoritos(times);
+            mostrarTimes(times);
 
             Console.WriteLine(texto);
             Console.ReadLine();
         }
 
-        static void mostrarIdade(int idade, int idadeAmigo, String nomeAmigo) {
+        static void mostrarIdade(int idade, int idadeAmigo, string nomeAmigo) {
             if (idadeAmigo > idade)
             {
-                Console.Write("\nOlá " + nomeAmigo + " Tudo bem com você?");
+                Console.Write($"\nOlá  {nomeAmigo}  Tudo bem com você?");
                 Console.WriteLine("\nVocê é mais velho que eu!!!");
             }
             else if (idadeAmigo < idade)
@@ -75,7 +78,7 @@ namespace HelloWorld
 
         }
 
-        static void seuTime(String timeAmigo)
+        static void seuTime(string timeAmigo)
         {
 
             if (timeAmigo != "Flamengo" && timeAmigo != "Vasco")
@@ -91,5 +94,43 @@ namespace HelloWorld
                 Console.WriteLine("\nVocê é uma pessoa de sorte, bem vindo a vida de vencedor!!!");
             }
         }
+
+        static void gerarPreco(float preco)
+        {
+            Console.WriteLine("\nQual o preço médio do ingresso do seu time??");
+            preco = int.Parse(Console.ReadLine());
+            float precoAbs = Math.Abs(preco);
+
+            if (precoAbs < 50f)
+            {
+                Console.WriteLine($"UAU, seu time cobra muito baratoooooo!!! Apenas R${precoAbs}");
+            } else
+            {
+                Console.WriteLine($"QUE CAROOOO R${precoAbs} é muita coisa");
+            }
+        }
+
+        static void cadastrotimesFavoritos(string[] times)
+        {
+            Console.WriteLine("\nAgora vamos falar sobre seus três time favoritos!!!");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"\nDigite o {i + 1}° time favorito!");
+                times[i]=Console.ReadLine();
+            }
+
+        }
+
+        static void mostrarTimes(string[] times)
+        {
+            Console.WriteLine("\n\nEntão estes são seus times favoritos:");
+            for(int i = 0; i < times.Length; i++)
+            {
+               Console.WriteLine($"\n{times[i]}");
+            }
+        }
+
+
+
     }
 }
